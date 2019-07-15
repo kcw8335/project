3d4f4a55bca6f20390a073b786e399c6c3e28af8
changwoo <note8335@naver.com>
2019-07-15 17:50:35 +0900

    community2

--- communityapp/models.py
+++ communityapp/models.py
@@ -15,7 +15,6 @@ class Notice(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
-    image = models.ImageField() # pip install pillow
 
     def __str__(self):
         return self.title

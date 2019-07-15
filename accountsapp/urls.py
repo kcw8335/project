f3e78c509badb7d3a0264c40b3462c982055fc55
changwoo <note8335@naver.com>
2019-07-15 18:50:04 +0900

    accounts

--- accountsapp/urls.py
+++ accountsapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.accountsapp, name="accountsapp"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+    path('signup/', views.signup, name="signup"),
 ]
--- accountsapp/views.py
+++ accountsapp/views.py
@@ -1,4 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def accountsapp(request):
     return render(request, 'accountsapp.html')
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': '이미 사용중인 아이디입니다.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('accountsapp')
+        else:
+            return render(request, 'signup.html', {'error': '패스워드가 동일해야 합니다.'})
+    else:
+        return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('accountsapp')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('accountsapp')
+    return render(request, 'signup.html')
+
+
+

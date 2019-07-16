dd1fab7d79f495549c8bc95b35cecb99d77628b7
changwoo <note8335@naver.com>
2019-07-16 16:05:51 +0900

    templates inheritance

--- communityapp/views.py
+++ communityapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.core.paginator import Paginator
 from .models import Community, Notice
+from django.contrib.auth.decorators import login_required
 
 def communityapp(request):
     return render(request, 'communityapp.html')
@@ -22,9 +23,11 @@ def communitydetail(request, community_id):
     communitydetail = get_object_or_404(Community, pk=community_id)
     return render(request, 'communitydetail.html', {'communitydetail':communitydetail})
 
+@login_required
 def communitywrite(request):
     return render(request, 'communitywrite.html')
 
+@login_required
 def communitycreate(request):
     community = Community()
     community.title = request.GET['title']

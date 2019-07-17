2b7b98f342cb8643b6143287cd535e7d8a4570c5
changwoo <note8335@naver.com>
2019-07-17 18:54:16 +0900

    comment

--- communityapp/urls.py
+++ communityapp/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
 
     path('community/write/', views.communitywrite, name="communitywrite"),
     path('community/create/', views.communitycreate, name="communitycreate"),
+
+    path('community/edit/<int:community_id>', views.communityedit, name="communityedit"),
+    path('community/delete/<int:community_id>', views.communitydelete, name="communitydelete"),
+
+    path('commentadd/<int:community_id>', views.commentadd, name="commentadd"),
+
+    path('commentedit/<int:comment_id>', views.commentedit, name="commentedit"),
+    path('commentdelete/<int:comment_id>', views.commentdelete, name="commentdelete"),
 ]

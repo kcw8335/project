7988f9084a195a5041ac21aa85746aaa1158fe2d
changwoo <note8335@naver.com>
2019-07-28 04:59:10 +0900

    settings

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6+z^at(#z!bs^=19ugqeu5a8l83k5@32^qp#nhksp#6_645#&^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.un9mdprsvj.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
